03d2259071e03ba27c95103935b8b60aac4d6298
David Gomes <davidrafagomes@gmail.com>
2013-01-11 23:20:44 +0000

    Window is now centered on the screen.

--- src/Game.cpp
+++ src/Game.cpp
@@ -9,13 +9,15 @@ void Game::setup()
   title = "2D Platformer";
   window.create(sf::VideoMode(800, 640, 1), title.c_str(), sf::Style::Titlebar | sf::Style::Close);
   window.setFramerateLimit(60);
-  lastTime = 0;
+  window.setPosition(sf::Vector2i(sf::VideoMode::getDesktopMode().width / 2 - 400,
+                                  sf::VideoMode::getDesktopMode().height / 2 - 320));
   windowFocused = true;
 
   /* Load arial font */
   arialFont.loadFromFile("res/arial.ttf");
 
   currentState = new PlayState(this);
+  lastTime = 0;
 }
 
 void Game::run()
